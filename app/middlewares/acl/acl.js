3a2529454304d141dc1493eca92f0883f75e16fb
pwwpcheng <pwwp@outlook.com>
2017-12-22 00:20:09 -0800

    Sanitize acl middleware

--- app/middlewares/acl/acl.js
+++ app/middlewares/acl/acl.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 acl.prototype.initialize = function() {
   return require('./init')(this)
 }
-acl.prototype.aclMiddleware = require('./middleware')
+acl.prototype.aclMiddleware = function() {
+  return require('./middleware')(this)
+}
 
 var _acl = new acl(new acl.mongodbBackend(mongoose.connection.db, 'acl_'))
 module.exports = _acl
--- app/middlewares/acl/middleware.js
+++ app/middlewares/acl/middleware.js
@@ -1,7 +1,4 @@
-//const _acl = require('./acl')
-//console.log(_acl)
-
-function aclMiddleware() {
+function aclMiddleware(acl) {
   return function (req, res, next) {
     // Uncomment the following line to bypass acl middleware
     //return next()
@@ -9,7 +6,6 @@ function aclMiddleware() {
     // Uncomment the following line to block all requests
     //return next(new Error("ACL Block in effect. All requests are blocked"))
 
-    var acl = req._acl
     acl.isAllowed(req.user._id.toString(), req.url, req.method, function(err, result) {
       if(err) {
         next(err)
